a32381c330b2d66d6a27e33072b7405d96c1c489
Your Name <a.krutick@gmail.com>
2024-07-15 22:15:39 +0200

    second build

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+﻿import React from "react";
+
+const Header = () => {
+    return (
+
+        <div>
+            
+
+        </div>
+
+    );
+};
+
+export default Header;
---
+++ src/components/button/button.jsx
@@ -0,0 +1,9 @@
+﻿import React from "react";
+
+const Button = (props) => {
+    return (
+        <button {...props} className={'button ' + props.className}/>
+    );
+};
+
+export default Button;
