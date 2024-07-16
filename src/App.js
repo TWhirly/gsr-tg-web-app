edee1f88522364833425ee078ed13e3621970dd5
Your Name <a.krutick@gmail.com>
2024-07-16 23:00:13 +0200

    restore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram.js";
+import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
-// import ProductList from "./components/ProductList/ProductList";
+import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
@@ -11,13 +11,13 @@ function App() {
 
     useEffect(() => {
         tg.ready();
-    }, [tg])
+    }, [])
 
     return (
         <div className="App">
             <Header />
             <Routes>
-                {/* <Route index element={<ProductList />}/> */}
+                <Route index element={<ProductList />}/>
                 <Route path={'form'} element={<Form />}/>
             </Routes>
         </div>
