a95f160e718456f00f5f02707377a32b52efaf38
Your Name <a.krutick@gmail.com>
2024-07-16 23:34:40 +0200

    index element try

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-                {/* <Route index element={<ProductList />}/> */}
-                <Route path={'form'} element={<Form />}/>
+                <Route index element={<Form />}/>
+                {/* <Route path={'form'} element={<Form />}/> */}
             </Routes>
         </div>
     );
