de635b1b0a34dd2824114718f06c243f6471f825
TWhirly <a.krutick@gmail.com>
2024-07-23 01:16:53 +0200

    table+

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import './Form.css';
 import { useTelegram } from "../../hooks/useTelegram";
 import { NumberField, Label, Group, Input, Button } from 'react-aria-components';
-import {Cell, Column, Row, Table, TableBody, TableHeader} from 'react-aria-components';
+import { Cell, Column, Row, Table, TableBody, TableHeader } from 'react-aria-components';
 
 const Form = () => {
     const [country, setCountry] = useState('');
@@ -82,49 +82,49 @@ const Form = () => {
                 <option value={'legal'}>Юр. лицо</option>
             </select>
             <Table aria-label="Files" selectionMode="multiple">
-  <TableHeader>
-    <Column>
-      <MyCheckbox slot="selection" />
-    </Column>
-    <Column isRowHeader>Name</Column>
-    <Column>Type</Column>
-    <Column>Date Modified</Column>
-  </TableHeader>
-  <TableBody>
-    <Row>
-      <Cell>
-        <MyCheckbox slot="selection" />
-      </Cell>
-      <Cell>Games</Cell>
-      <Cell>File folder</Cell>
-      <Cell>6/7/2020</Cell>
-    </Row>
-    <Row>
-      <Cell>
-        <MyCheckbox slot="selection" />
-      </Cell>
-      <Cell>Program Files</Cell>
-      <Cell>File folder</Cell>
-      <Cell>4/7/2021</Cell>
-    </Row>
-    <Row>
-      <Cell>
-        <MyCheckbox slot="selection" />
-      </Cell>
-      <Cell>bootmgr</Cell>
-      <Cell>System file</Cell>
-      <Cell>11/20/2010</Cell>
-    </Row>
-    <Row>
-      <Cell>
-        <MyCheckbox slot="selection" />
-      </Cell>
-      <Cell>log.txt</Cell>
-      <Cell>Text Document</Cell>
-      <Cell>1/18/2016</Cell>
-    </Row>
-  </TableBody>
-</Table>
+                <TableHeader>
+                    <Column>
+                        <MyCheckbox slot="selection" />
+                    </Column>
+                    <Column isRowHeader>Name</Column>
+                    <Column>Type</Column>
+                    <Column>Date Modified</Column>
+                </TableHeader>
+                <TableBody>
+                    <Row>
+                        <Cell>
+                            <MyCheckbox slot="selection" />
+                        </Cell>
+                        <Cell>Games</Cell>
+                        <Cell>File folder</Cell>
+                        <Cell>6/7/2020</Cell>
+                    </Row>
+                    <Row>
+                        <Cell>
+                            <MyCheckbox slot="selection" />
+                        </Cell>
+                        <Cell>Program Files</Cell>
+                        <Cell>File folder</Cell>
+                        <Cell>4/7/2021</Cell>
+                    </Row>
+                    <Row>
+                        <Cell>
+                            <MyCheckbox slot="selection" />
+                        </Cell>
+                        <Cell>bootmgr</Cell>
+                        <Cell>System file</Cell>
+                        <Cell>11/20/2010</Cell>
+                    </Row>
+                    <Row>
+                        <Cell>
+                            <MyCheckbox slot="selection" />
+                        </Cell>
+                        <Cell>log.txt</Cell>
+                        <Cell>Text Document</Cell>
+                        <Cell>1/18/2016</Cell>
+                    </Row>
+                </TableBody>
+            </Table>
         </div>
     );
 };
