fc61447299846040040d880a16966ca91114722b
TWhirly <a.krutick@gmail.com>
2024-07-25 01:03:57 +0200

    form wo table

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -83,34 +83,7 @@ const Form = () => {
                 <option value={'legal'}>Юр. лицо</option>
             </select>
             
-            <Table aria-label="Files" >
-                <TableHeader>
-
-                    <Column>Type</Column>
-                    <Column>Date Modified</Column>
-                </TableHeader>
-                <TableBody>
-                    <Row>
-                        <Cell>File folder</Cell>
-                        <Cell>6/7/2020</Cell>
-                    </Row>
-                    <Row>
-
-                        <Cell>File folder</Cell>
-                        <Cell>4/7/2021</Cell>
-                    </Row>
-                    <Row>
-
-                        <Cell>System file</Cell>
-                        <Cell>11/20/2010</Cell>
-                    </Row>
-                    <Row>
-
-                        <Cell>Text Document</Cell>
-                        <Cell>1/18/2016</Cell>
-                    </Row>
-                </TableBody>
-            </Table>
+            
         </div>
     );
 };
