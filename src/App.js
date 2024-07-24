7d7b30463f55a103286fcac42d147f5b48146938
TWhirly <a.krutick@gmail.com>
2024-07-25 01:10:53 +0200

    my table

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import Header from "./components/Header/Header";
 import {Route, Routes} from 'react-router-dom'
 // import {Cell, Column, Row, Table, TableBody, TableHeader} from 'react-aria-components';
 // import ProductList from "./components/ProductList/ProductList";
-import Form from "./components/Form/Form";
+// import Form from "./components/Form/Form";
 
 
 // import { Table } from 'react-aria-components';
-// import MyTable from "./components/Table/table";
+import MyTable from "./components/Table/table";
 
 function App() {
     // eslint-disable-next-line no-unused-vars
@@ -23,7 +23,7 @@ function App() {
         <div className="App">
             <Header />
             <Routes>
-                <Route index element={<Form />}/>
+                <Route index element={<MyTable />}/>
                 {/* <Route path={'form'} element={<Form />}/> */}
             </Routes>
         </div>
