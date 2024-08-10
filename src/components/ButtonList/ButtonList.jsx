bd5d9f7292f496cfbc97da45d934fc551614fc19
Your Name <a.krutick@gmail.com>
2024-08-11 00:54:19 +0200

    appsheet menu

--- src/components/ButtonList/ButtonList.jsx
+++ src/components/ButtonList/ButtonList.jsx
@@ -5,18 +5,38 @@ import { useTelegram } from "../../hooks/useTelegram";
 import { useCallback, useEffect } from "react";
 import { Button } from "../button/button"
 
-const buttons = [
-    { id: '1', description: 'Синего цвета, прямые', title: 'Джинсы', price: 5000 },
-    { id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая' },
-    { id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые' },
-    { id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая' },
-    { id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые' },
-    { id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая' },
-    { id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые' },
-    { id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая' },
-]
+// const buttons = [
+//     { id: '1', description: 'Синего цвета, прямые', title: Date.now(), price: 5000 },
+//     { id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая' },
+//     { id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые' },
+//     { id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая' },
+//     { id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые' },
+//     { id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая' },
+//     { id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые' },
+//     { id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая' },
+// ]
+
+// const buttons = Promise.resolve(await (await fetch('http://localhost:8000/trucks-data')).json())
+
+
+
+
+//console.log(buttons)
 
 const ButtonList = () => {
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        buttons();
+    }, []);
+
+    const buttons = async () => {
+        const response = await fetch('http://localhost:8000/menu'); // Генерируем объект Response
+        const jVal = await response.json(); // Парсим тело ответа
+        setData(jVal);
+    }
+
+
     const { tg, queryId } = useTelegram();
 
     const handleClick = (item) => {
@@ -26,7 +46,7 @@ const ButtonList = () => {
 
         })
         alert('clicked ' + item);
-        
+
 
     }
 
@@ -51,26 +71,35 @@ const ButtonList = () => {
     //             />
     //         ))}
 
+    //     </div>
+    // );
+    // const listItems = buttons.map(item => item.title)
+    // return (
+    //     <div className={'list'} >
 
+    //         {buttons.map(item => ( <button className={'btn'} onClick={() => handleClick(item.driver)}>   {item.driver} </button>)) }
 
 
     //     </div>
     // );
-    // const listItems = buttons.map(item => item.title)
+
     return (
         <div className={'list'} >
 
-            {buttons.map(item => ( <button className={'btn'} onClick={() => handleClick(item.title)}>   {item.title} </button>)) }
-           
-
-
-
-
-
+            {data.map(item => ( <button className={'btn'} onClick={() => handleClick(item.action)}>   {item.action} </button>)) }
 
 
         </div>
     );
+
+    // return (
+        
+    //     <div>
+    //         <div>123</div>
+    //         {JSON.stringify(data)}
+
+    //     </div>
+    // );
 
 }
 
