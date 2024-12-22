8c567f54dd518a7ea63622a62b31ffbc5e6156da
TWhirly <a.krutick@gmail.com>
2024-12-22 22:36:50 +0200

    fuelIntake almost ready

--- src/components/fuelIntake/FuelIntake.jsx
+++ src/components/fuelIntake/FuelIntake.jsx
@@ -1,13 +1,14 @@
-import React, { useCallback, useEffect, useState, useLayoutEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useState, useLayoutEffect, useRef} from 'react';
 import styles from './fuelIntake.module.css';
 import { localUrl } from '../../localSettings.js'
 import 'animate.css';
-import { useNavigate, useHistory } from "react-router-dom";
+import { useNavigate, useHistory} from "react-router-dom";
 import { NumberField, Label, Group, Input, Button, Cell, Column, Row, Table, TableBody, TableHeader } from 'react-aria-components';
 import { useLinkProps } from '@react-aria/utils';
 import { useTelegram } from "../../hooks/useTelegram";
 import { type } from '@testing-library/user-event/dist/type/index.js';
-
+import {  Element, Events, animateScroll as scroll, Link  } from 'react-scroll';
+const deepEqual = require('deep-equal');
 
 // import { Cell, Column, Row, Table, TableBody, TableHeader } from 'react-aria-components';
 
@@ -17,8 +18,7 @@ const APIURL = localUrl.APIURL;
 
 const FuelIntake = () => {
     const navigate = useNavigate();
-    const inputRef = useRef(null);
-    const nextInputRef = useRef(null);
+    const myRef = useRef();
 
     const fetchFormFields = async () => {
         const response = await fetch(APIURL + '/fuelIntake', {
@@ -51,12 +51,11 @@ const FuelIntake = () => {
     const [cal, setCal] = useState({});
     const [recievedFormData, setRecievedFormData] = useState({});
     const [formDataInputs, setFormDataInputs] = useState({});
-    const [allFieldsFilled, setIsFormComplete] = useState(false);
+    const [haveChanges, setIsChangesExist] = useState(false);
     const [calibLoad, setCalibLoad] = useState(false)
     const [formLoad, setFormLoad] = useState(false)
     const [densTempShow, setDensTempshow] = useState(new Map())
     const [toggleState, setToggleState] = useState(false);
-    const contentRef = useRef(null);
 
 
 
@@ -89,6 +88,7 @@ const FuelIntake = () => {
             }, {});
             setFormData(initialFormData);
             setRecievedFormData(initialFormData);
+            setFormDataInputs(initialFormData)
             setFields(fetchedFields);
             setFormLoad(true);
 
@@ -137,6 +137,13 @@ const FuelIntake = () => {
                         awaitH: calcAwaitH(value, id),
                     },
                 }));
+                setFormDataInputs(prevData => ({
+                    ...prevData,
+                    [id]: {
+                        ...prevData[id],
+                        awaitH: calcAwaitH(value, id),
+                    },
+                }));
             })
         }
     }, [calibLoad, formLoad, fields])
@@ -145,11 +152,6 @@ const FuelIntake = () => {
         setDensTempshow(densTempShow)
     }, [densTempShow, toggleState])
 
-    useEffect(() => {
-        if (contentRef.current) {
-            !contentRef.current.style.height == Hide ? `${contentRef.current.scrollHeight}px` : `'0'`;
-        }
-    }, [densTempShow]);
 
     const calcAwaitH = (h, id, tank, waybill) => {
         // console.log('calc')
@@ -202,11 +204,21 @@ const FuelIntake = () => {
         else {
             densTempShow.set(id, true)
         }
+        
+        
     }
 
     const clearOnFocus = (e) => {
+        setRecievedFormData(prevData => ({
+            ...prevData,
+            [id]: {
+                ...prevData[id],
+                [key]: value,
+            },
+        }))
         const id = e.target.id
         const key = e.target.name
+        const value = e.target.value
         if (key.substring(0, 1) == 'd') {
             console.log('key is d started')
             setFormData(prevData => ({
@@ -260,13 +272,22 @@ const FuelIntake = () => {
         const id = e.target.id
         const key = e.target.name
         const tValue = e.target.value
+        console.log('tValue', tValue)
+        const oldValue = recievedFormData[id][key]
         let value
+       
         if (!tValue.includes(',') && tValue.length > 0) {
             value = tValue + ',0'
         }
-        else {
+        if(tValue.length === 0){
+            console.log('9')
+            value = oldValue
+        }
+        if(tValue.includes(',') && tValue.length > 0) {
+            console.log('hm')
             value = tValue
         }
+        console.log('vv is', value)
         setFormData(prevData => ({
             ...prevData,
             [id]: {
@@ -308,6 +329,8 @@ const FuelIntake = () => {
                     awaitH: calcAwaitH(tValue, id),
                 },
             }))
+
+           
         }
         else {
             setFormData(prevData => ({
@@ -397,11 +420,20 @@ const FuelIntake = () => {
 
 
     useEffect(() => {
-        // Проверяем, заполнены ли все поля формы
-        const allFieldsFilled = fields.every(field => (formDataInputs[field.id])) && Object.keys(formData).length > 0;
-
-        allFieldsFilled && setIsFormComplete(allFieldsFilled);
-    }, [formDataInputs, fields]);
+        var loaded = []
+        var current = []
+        Object.keys(formData).forEach(key => {
+            Object.keys(formData[key]).forEach(key2 => {
+                if(key2 == 'dAfter' || key2 == 'dBefore' || key2 == 'dTruck' || key2 == 'hAfter' || key2 == 'hBefore' || key2 == 'tAfter'
+                    || key2 == 'tBefore' || key2 == 'tTruck'){
+                    current.push(+(formData[key][key2].toString().replace(',','.')))
+                    loaded.push(+(formDataInputs[key][key2].toString().replace(',','.')))
+                }
+            })
+        })
+        setIsChangesExist(current.join(' ') !== loaded.join(' '))
+    }
+    , [formData, formDataInputs]);
 
     const handleSubmit = () => {
         console.log(allFieldsFilled);
@@ -422,19 +454,21 @@ const FuelIntake = () => {
 
 
     }
-    console.log('render', formData);
-    console.log('show', densTempShow)
+    console.log('render', formData, formDataInputs);
+    console.log('is changes exist', haveChanges)
     if (calibLoad && formLoad) {
 
 
         return (
             <div className={styles.container}>
-                <div className={styles.subheader}>Поступления НП сегодня, {(new Date).toLocaleDateString()}</div>
+                <Element name={"start"} className={styles.subheader} id={'start'}>Поступления НП сегодня, {(new Date).toLocaleDateString()} </Element>
                 <div className={styles.intakesContainer}>{fields.map((field) => {
                     return (
                         <div className={styles.intakeBlock} key={field.id}>
                             <div className={styles.intakeData}>
-                                <div className={styles.intakeDataHeader}>Данные ТТН/Разнарядки</div>
+                                <div className={styles.intakeDataHeader}
+                                    name={field.id + 'start'}
+                                >Данные ТТН/Разнарядки</div>
                                 <div className={styles.fueltype}>{field.fuel !== 'ДТ' ? 'АИ-' + field.fuel : field.fuel} </div>
                                 <div className={styles.tank}>Резервуар № {field.tank}</div>
                                 <div className={styles.driver}>Водитель: {field.driver}</div>
@@ -443,6 +477,7 @@ const FuelIntake = () => {
                                 <div className={styles.waybill}>Объем по накладной: {field.waybill}</div>
                             </div>
                             <div className={styles.measuresData}>
+                                <div className={styles.intakeHeader}>Замеры</div>
                                 <div className={styles.hBefore}>Уровень до слива, см</div>
                                 <div className={styles.inputline}>
 
@@ -459,8 +494,7 @@ const FuelIntake = () => {
                                         onChange={handleChange}
                                         maxLength={5}
                                         onFocus={clearOnFocus}
-                                        onBlur={handleBlurH}
-                                    />
+                                        onBlur={handleBlurH} />
                                     <button className={styles.button}
                                         id={field.id}
                                         name='hBefore'
@@ -472,9 +506,9 @@ const FuelIntake = () => {
                                         tabIndex="-1"
                                         value={formData[field.id]['hBefore']} onClick={(e) => handleChange(e, 0.1)}>+</button>
                                 </div>
-                                <div className={styles.awaitH}>{formData[field.id]['awaitH'] ? 'Ожидаемый уровень: ' + (formData[field.id]['awaitH']).toFixed(1).replace('.', ',') :
+                                <div className={`${styles.awaitH} + ${(formData[field.id]['awaitH'] ? '' : styles.Overflow)}`}>{formData[field.id]['awaitH'] ? 'Ожидаемый уровень: ' + (formData[field.id]['awaitH']).toFixed(1).replace('.', ',') :
                                     "Превышена вместимость!"}</div>
-                                <div >
+                                <div>
                                     <div className={styles.hBefore}>Уровень после слива, см</div>
                                     <div className={styles.inputline}>
                                         <input
@@ -489,8 +523,7 @@ const FuelIntake = () => {
                                             maxLength={5}
                                             onChange={handleChange}
                                             onFocus={clearOnFocus}
-                                            onBlur={handleBlurH}
-                                        />
+                                            onBlur={handleBlurH} />
                                         <button
                                             className={styles.button}
                                             id={field.id}
@@ -506,23 +539,24 @@ const FuelIntake = () => {
                                     </div>
                                 </div>
                             </div>
-                            <div
+                            <Link to={!densTempShow.get(field.id) ? field.id + "dens" : field.id + "start"} smooth={true} duration={500}
+                                id={field.id}
+                                name={field.id + 'dens'}
                                 className={styles.DensTempDataHeader}
-                                id={field.id}
-                                onClick={hadleClickDensTempShow} >
+                                onClick={hadleClickDensTempShow}>
                                 Плотность и температура
-                            </div>
+                            </Link>
                             <div
                                 id={field.id}
-                                className={`${styles.densTempBlock} + ${(!densTempShow.get(field.id) ? styles.Hide : '')}`} ref={contentRef}>
+                                className={`${styles.densTempBlock} + ${(!densTempShow.get(field.id) ? styles.Hide : '')}`}>
 
                                 <div className={styles.farmDataHeader}>
 
                                     {'По данным нефтебазы: ' + formData[field.id]['dFarm'].toString().replace('.', ',')} {(formData[field.id]['tFarm'] > 0 ? '+' + formData[field.id]['tFarm'] :
                                         formData[field.id]['tFarm']) + '°'}</div>
                                 <div>
-                                    <div>В АЦ:</div>
-                                    <div className={styles.inputline}>
+                                    <div className={styles.waybill}>В АЦ:</div>
+                                    <div className={styles.inputlineDens}>
                                         <input
                                             className={styles.input}
                                             id={field.id}
@@ -533,8 +567,7 @@ const FuelIntake = () => {
                                             maxLength={5}
                                             onChange={handleChangeDens}
                                             onFocus={clearOnFocus}
-                                            onBlur={handleBlurD}
-                                        />
+                                            onBlur={handleBlurD} />
                                         <button
                                             className={styles.button}
                                             id={field.id}
@@ -548,7 +581,7 @@ const FuelIntake = () => {
                                             tabIndex="-1"
                                             value={formData[field.id]['dTruck']} onClick={(e) => handleChangeDens(e, 0.001)}>+</button>
                                     </div>
-                                    <div className={styles.inputline}>
+                                    <div className={styles.inputlineDens}>
                                         <input
                                             className={styles.input}
                                             id={field.id}
@@ -560,8 +593,7 @@ const FuelIntake = () => {
                                             max={40}
                                             maxLength={2}
                                             onFocus={clearOnFocus}
-                                            onChange={handleChangeTemp}
-                                        />
+                                            onChange={handleChangeTemp} />
                                         <button
                                             className={styles.button}
                                             id={field.id}
@@ -577,8 +609,8 @@ const FuelIntake = () => {
                                     </div>
                                 </div>
                                 <div>
-                                    <div>В резервуаре до слива:</div>
-                                    <div className={styles.inputline}>
+                                    <div className={styles.waybill}>В резервуаре до слива:</div>
+                                    <div className={styles.inputlineDens}>
                                         <input
                                             className={styles.input}
                                             id={field.id}
@@ -589,8 +621,7 @@ const FuelIntake = () => {
                                             maxLength={5}
                                             onChange={handleChangeDens}
                                             onFocus={clearOnFocus}
-                                            onBlur={handleBlurD}
-                                        />
+                                            onBlur={handleBlurD} />
                                         <button
                                             className={styles.button}
                                             id={field.id}
@@ -616,8 +647,7 @@ const FuelIntake = () => {
                                             max={40}
                                             maxLength={2}
                                             onFocus={clearOnFocus}
-                                            onChange={handleChangeTemp}
-                                        />
+                                            onChange={handleChangeTemp} />
                                         <button
                                             className={styles.button}
                                             id={field.id}
@@ -633,8 +663,8 @@ const FuelIntake = () => {
                                     </div>
                                 </div>
                                 <div>
-                                    <div>В резервуаре после слива:</div>
-                                    <div className={styles.inputline}>
+                                    <div className={styles.waybill} name={'aa'}>В резервуаре после слива:</div>
+                                    <div className={styles.inputlineDens}>
                                         <input
                                             className={styles.input}
                                             id={field.id}
@@ -645,8 +675,7 @@ const FuelIntake = () => {
                                             maxLength={5}
                                             onChange={handleChangeDens}
                                             onFocus={clearOnFocus}
-                                            onBlur={handleBlurD}
-                                        />
+                                            onBlur={handleBlurD} />
                                         <button
                                             className={styles.button}
                                             id={field.id}
@@ -672,8 +701,7 @@ const FuelIntake = () => {
                                             max={40}
                                             maxLength={2}
                                             onFocus={clearOnFocus}
-                                            onChange={handleChangeTemp}
-                                        />
+                                            onChange={handleChangeTemp} />
                                         <button
                                             className={styles.button}
                                             id={field.id}
@@ -692,10 +720,14 @@ const FuelIntake = () => {
 
 
                         </div>
+                       
                     )
-                })}</div>
+                })}
+                
+                </div>
+                {(haveChanges && <Button onPress={handleSubmit} className={styles.submit}>Отправить</Button>)}
             </div>
-
+    
         )
     }
 
